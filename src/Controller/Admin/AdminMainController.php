965d1a8f3ca4df39b6d1351714db3a807f3c0427
Evgeniy Baraulya <evgeniy.baraulya@gmail.com>
2020-09-09 22:43:44 +0300

    #10.1Add possibility to block/unblock users with flash message

--- src/Controller/Admin/AdminMainController.php
+++ src/Controller/Admin/AdminMainController.php
@@ -4,6 +4,7 @@
 namespace App\Controller\Admin;
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -37,9 +38,31 @@ class AdminMainController extends AdminBaseController
         $em=$this->getDoctrine()->getManager();
         $em->remove($user);
         $em->flush();
-
+        $this->addFlash('notification', 'User was deleted');
         return $this->redirect($this->generateUrl("admin_users"));
     }
 
+    /**
+     * @Route("/admin/users/block/{id}", name = "user_blocking")
+     */
+    public function block(User $user){
+        $em=$this->getDoctrine()->getManager();
+        $user=$em->getRepository(User::class)->find($user);
+        $user->setStatus('blocked');
+        $em->flush();
+        $this->addFlash('notification', 'User was blocked');
+        return $this->redirect($this->generateUrl("admin_users"));
+    }
 
+    /**
+     * @Route("/admin/users/unblock/{id}", name = "user_unblocking")
+     */
+    public function unblock(User $user){
+        $em=$this->getDoctrine()->getManager();
+        $user=$em->getRepository(User::class)->find($user);
+        $user->setStatus('active');
+        $em->flush();
+        $this->addFlash('notification', 'User was unblocked');
+        return $this->redirect($this->generateUrl("admin_users"));
+    }
 }
